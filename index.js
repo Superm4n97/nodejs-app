ccb1f18b133d5e105ebda5d971642b7f0cd9b7d1
rasel <rasel@appscode.com>
2025-11-03 14:28:55 +0600

    Add installer
    Signed-off-by: rasel <rasel@appscode.com>

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const port = process.env.PORT || 3000;
 app.get('/health', (req, res) => res.send('OK'));
 
 app.get('/', (req, res) => {
-  res.json({ message: "Hello from Node.js app running on Kubernetes!" });
+    res.json({ message: "Hello from Node.js app running on Kubernetes!" });
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+const server = app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
+server.on('error', err => {
+    if (err.code === 'EADDRINUSE') {
+        console.error(`Port ${port} is already in use. Try a different one.`);
+        process.exit(1);
+    } else {
+        throw err;
+    }
 });
